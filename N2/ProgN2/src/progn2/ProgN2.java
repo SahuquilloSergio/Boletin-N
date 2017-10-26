98b37bac375e7d56449858c5448de93e8b9ff504
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-26 09:07:38 +0200

    Commit Inicial

---
+++ N2/ProgN2/src/progn2/ProgN2.java
@@ -0,0 +1,13 @@
+package progn2;
+
+/**
+ *
+ * @author Sergio "Sauky" Sahuquillo
+ */
+public class ProgN2 {
+
+    public static void main(String[] args) {
+        System.out.println("Copernico el Puto Amo");
+    }
+    
+}
